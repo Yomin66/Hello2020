ada261b349a8cadeb8395d1fcb93c5bf57c31e59
student <s1081707@gm.pu.edu.tw>
2021-03-19 13:59:54 +0800

    他人加入螢幕觸控方法

--- app/src/main/java/com/example/hello2020/MainActivity.kt
+++ app/src/main/java/com/example/hello2020/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.hello2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +23,11 @@ class MainActivity : AppCompatActivity() {
     fun TestDEV(){
         var txv:TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Dev分支"
+    }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "tcyangpu修改螢幕觸控"
+        return true
     }
 
 }
